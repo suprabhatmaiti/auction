5fe878b3751a77dca3e952f401244588f34266f0
Suprabhat Maiti <maitisuprabhat@gmail.com>
2025-12-04 19:13:56 +0530

    add admin home page

--- src/client/pages/AdminPage/AdminHomePage.jsx
+++ src/client/pages/AdminPage/AdminHomePage.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import Body from "./Body";
 
 export default function AdminHomePage() {
-  return <div>AdminHomePage</div>;
+  return (
+    <div className="flex">
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Body />
+      </div>
+    </div>
+  );
 }
---
+++ src/client/pages/AdminPage/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="border-r ">
+      <div>
+        <div>Admin Name</div>
+        <div>Dashboard</div>
+      </div>
+      <div>Logout</div>
+    </div>
+  );
+}
